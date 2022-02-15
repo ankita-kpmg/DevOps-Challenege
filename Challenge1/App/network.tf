db2fe9f43c32e9422894ad6d3c8a59da309ddcf9
ankita-kpmg <99559131+ankita-kpmg@users.noreply.github.com>
2022-02-15 22:05:04 +0530

    network.tf

--- Challenge1/App/network.tf
+++ Challenge1/App/network.tf
@@ -29,60 +29,7 @@ resource "aws_subnet" "app-subnet-2" {
     Name = "Application-2b"
   }
 }
-#___________App Security Group
-module "app_sg" {
-  source = ""
-  name        = "${var.app_sg_name}"
-  description = "${var.app_sg_description}"
-  vpc_id      = "${data.aws_vpc.gi_vpc.id}"
-  computed_ingress_with_cidr_blocks = [
-    {
-      from_port   = "${var.port_3333}"
-      to_port     = "${var.port_3333}"
-      protocol    = "tcp"
-      description = "SIP"
-      cidr_blocks = "${var.cidr_ranges_all}"
-    }, 
-    ]
- number_of_computed_ingress_with_cidr_blocks = 1
-  computed_egress_with_cidr_blocks = [
-    {
-      from_port   = "${var.port_1111}"
-      to_port     = "${var.port_1111}"
-      protocol    = "tcp"
-      description = "SIP secured"
-      cidr_blocks = "${var.cidr_dev}"
-    }, 
-    ]
-    number_of_computed_egress_with_cidr_blocks = 1
-    tags {
-    Name                   = "${var.web_alb_sg_name}"
-  ....
-      ....
-  }
-} 
-  
-  module "app_alb_sg" {
-  source = "git::github_terraform_modules"
-  name        = "${var.app_alb_sg_name}"
-  description = "${var.app_alb_sg_description}"
-  vpc_id      = "${data.aws_vpc.gi_vpc.id}"
-  computed_ingress_with_cidr_blocks = [
-    {
-      from_port   = "${var.port_9999}"
-      to_port     = "${var.port_9999}"
-      protocol    = "tcp"
-      description = " XXX Access"
-      cidr_blocks = "${var.cidr_9999}"
-    },
-    ]
-  number_of_computed_ingress_with_cidr_blocks = 1 
-  tags {
-    Name                   = "${var.app_alb_sg_name}"
-  ....
-      ....
-  }
-} 
+
 #___________App server Security Group__________________
 resource "aws_security_group" "appserver-sg" {
   name        = "Appserver-SG"
